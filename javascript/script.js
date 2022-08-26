c4a3437958310145b7b3ddca7fd07362f2bf1158
svalvastic <assogames2@gmail.com>
2022-08-26 17:25:34 +0200

    ✨ feat(script): integration with ui

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,13 @@
 const pcLaunch = [];
 const userLaunch = [];
 const plays = [0, 0]; //indice 0 per PLAYER indice 1 per COMPUTER
-for (let i = 0; i < 3; i++) {
-  let winner;
+let round = 0; //numero di round giocati
+let percent = 50; //percentuale della barra
+const button = document.getElementById("diceLauncher");
+const barGreen = document.getElementById("barGreen");
 
+
+button.addEventListener("click", function () {
   let pcCurrent = Math.floor(Math.random() * 6) + 1;
   console.log("Il computer ha lanciato", pcCurrent);
   pcLaunch.push(pcCurrent);
@@ -11,23 +15,69 @@ for (let i = 0; i < 3; i++) {
   let userCurrent = Math.floor(Math.random() * 6) + 1;
   console.log("Hai lanciato", userCurrent);
   userLaunch.push(userLaunch);
-  if (userCurrent === pcCurrent && plays[0] === 1 && plays[1] === 1) {
-    i--;
-  } else if (userCurrent > pcCurrent) {
+
+  if (userCurrent === pcCurrent) {
+    round--; //IN CASO DI PAREGGIO E' PREVISTO UN ALTRO LANCIO
+  }
+  else if (userCurrent > pcCurrent) {//valutazione dei lanci
+    percent += 25;
+    barGreen.style.width = percent + "%";
+
     plays[0] += 1;
-  } else {
+  }
+  else {
+    percent -= 25;
+    barGreen.style.width = percent + "%";
     plays[1] += 1;
   }
 
+
+
   if (plays[0] === 2 || plays[1] === 2) {
-    i++;
+    ++round; //IN CASO DI NETTA VITTORIA LA PARTITA NON ARRIVA A 3 QUINDI AUMENTA I ROUND
+    if (plays[0] === 2) {
+      percent += 25;//AGGIORNAMENTO DELLA BARRA
+      barGreen.style.width = percent + "%";
+    } else {
+      percent -= 25;
+      barGreen.style.width = percent + "%";
+    }
   }
 
-  console.log("USER:", plays[0], " PC:", plays[1]);
-}
 
-if (plays[0] > plays[1]) {
-  console.log("HAI VINTO");
-} else {
-  console.log("HAI PERSO");
-}
+
+
+  console.log("USER:", plays[0], " PC:", plays[1]);
+
+
+  if (round >= 2) {//CHIUSURA DELLA PARTITA 
+    console.log("Abbiamo un vincitore");
+    button.setAttribute("disabled", "");
+
+    const announcement = document.getElementById("result");
+    const text = document.createElement("h1");
+    let result = "loose";
+      
+    if (plays[0] > plays[1]) {
+      result= "victory";
+      }
+      
+    text.classList.add(result);
+    text.append(result);
+    announcement.append(text);
+      
+
+  } else {
+    round++;
+  }
+
+
+
+
+
+
+  document.getElementById("diceUser").innerHTML = userCurrent;
+  document.getElementById("dicePc").innerHTML = pcCurrent;
+  document.getElementById("userScore").innerHTML = plays[0];
+  document.getElementById("pcScore").innerHTML = plays[1];
+});
